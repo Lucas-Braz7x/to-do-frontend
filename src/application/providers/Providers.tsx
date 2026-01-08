910a5b3d9d581c9db3419b34900619f6e7287439
ldutra <ldutra@rpc.com.br>
2026-01-07 23:59:04 -0300

    feat: add providers with auth and loading integration

---
+++ src/application/providers/Providers.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { useEffect } from 'react';
+import { AuthProvider } from '../contexts/AuthContext';
+import {
+  LoadingProvider,
+  useLoading,
+  setLoadingCallbacks,
+} from '../contexts/LoadingContext';
+import { LoadingOverlay } from '@/src/components/organisms/LoadingOverlay';
+
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+function LoadingInitializer({ children }: { children: React.ReactNode }) {
+  const { startLoading, stopLoading } = useLoading();
+
+  useEffect(() => {
+    setLoadingCallbacks({
+      start: startLoading,
+      stop: stopLoading,
+    });
+  }, [startLoading, stopLoading]);
+
+  return <>{children}</>;
+}
+
+export function Providers({ children }: ProvidersProps) {
+  return (
+    <LoadingProvider>
+      <LoadingInitializer>
+        <AuthProvider>{children}</AuthProvider>
+        <LoadingOverlay />
+      </LoadingInitializer>
+    </LoadingProvider>
+  );
+}
